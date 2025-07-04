39a507c0c1286b6f95e37aa52374e16d798093a5
abril troncoso <abriltroncoso108@gmail.com>
2025-07-04 20:30:48 -0300

    modifique layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
 export class App {
   protected title = 'republica';
   protected logo = 'assets/images/logo-republica.png';
+  protected productos = [
+  { img: 'assets/gorra.png' },
+  { img: 'assets/eventos.png' },
+  { img: 'assets/comida.png' },
+   { img: 'assets/cartel.png' },
+];
 }
