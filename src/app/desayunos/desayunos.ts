dd26965bbc8a7eae2758272df3c4e896e58fbdba
abril troncoso <abriltroncoso108@gmail.com>
2025-07-21 13:12:52 -0300

    agregue backend

--- src/app/desayunos/desayunos.ts
+++ src/app/desayunos/desayunos.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewChecked, AfterViewInit } from '@angular/core';
+declare var bootstrap: any;
 @Component({
   selector: 'app-desayunos',
   standalone: false,
   templateUrl: './desayunos.html',
   styleUrl: './desayunos.scss'
 })
-export class Desayunos {
-
+export class Desayunos implements AfterViewInit {
+  ngAfterViewInit() {
+    const carouselElement = document.getElementById('carouselDesayunos');
+    if (carouselElement) {
+      new bootstrap.Carousel(carouselElement, {
+        interval: 3000,
+        ride: 'carousel'
+      });
+    }
+  }
 }
--- src/app/eventos/eventos.ts
+++ src/app/eventos/eventos.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component ,AfterViewInit } from '@angular/core';
+declare var bootstrap: any;
 @Component({
   selector: 'app-eventos',
   standalone: false,
   templateUrl: './eventos.html',
   styleUrl: './eventos.scss'
 })
-export class Eventos {
-
+export class Eventos implements AfterViewInit {
+  ngAfterViewInit() {
+    const carouselElement = document.getElementById('carouselCumple');
+    if (carouselElement) {
+      new bootstrap.Carousel(carouselElement, {
+        interval: 3000,
+        ride: 'carousel'
+      });
+    }
+  }
 }
